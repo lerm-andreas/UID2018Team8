dbe7cf1a97c9b6d6b39f9b43a7434e69ee72d2ec
lerm-andreas <lerm.andreas@gmail.com>
2019-01-11 21:28:41 +0200

    eliminated some lines that appeared due to stash

--- src/components/Main/EventsPage/EventsList/Event/InviteEvent.js
+++ src/components/Main/EventsPage/EventsList/Event/InviteEvent.js
@@ -1,9 +1,9 @@
 import React from 'react';
-import {Button} from '@material-ui/core';
+import { Button } from '@material-ui/core';
 import {withStyles} from '@material-ui/core/styles';
+import CheckboxListSecondary from "./CheckboxList";
 import DialogWindow from "../../../../Utils/DialogWindow";
 import DialogWindowInvite from "./DialogWindowInvite";
-
 const styles = theme => ({
     // fab: {
     //     margin: theme.spacing.unit,
@@ -23,17 +23,17 @@ class InviteEvent extends React.Component {
     constructor(props) {
 
         super(props);
-        this.state = {
-            showInviteBox: false,
+        this.state={
+            showInviteBox : false,
             showInviteButton: false,
             showConfirmationDialog: false,
             buttonText: 'Join event',
             buttonColor: "primary"
-        };
+        }
         this.handleInviteBoxOpen = this.handleInviteBoxOpen.bind(this);
         this.handleInviteBoxClose = this.handleInviteBoxClose.bind(this);
-        this.handleConfirmationDialogClose = this.handleConfirmationDialogClose.bind(this);
-        this.handleConfirmationDialogOpen = this.handleConfirmationDialogOpen.bind(this);
+        this.handleConfirmationDialogClose=this.handleConfirmationDialogClose.bind(this);
+        this.handleConfirmationDialogOpen= this.handleConfirmationDialogOpen.bind(this);
         this.handleJoinButtonClicked = this.handleJoinButtonClicked.bind(this);
     }
 
@@ -54,53 +54,37 @@ class InviteEvent extends React.Component {
     };
 
     handleJoinButtonClicked = () => {
-        if (this.state.buttonText === 'Join event') {
-            this.setState({buttonText: 'Going', buttonColor: "secondary", showInviteButton: true})
+        if(this.state.buttonText=='Join event'){
+        this.setState({buttonText:'Going',buttonColor:"secondary",showInviteButton: true})
         }
-        else {
-            this.setState({
-                buttonText: 'Join event',
-                buttonColor: "primary",
-                showInviteButton: false
-            })
+        else{
+            this.setState({buttonText:'Join event',buttonColor:"primary",showInviteButton: false})
         }
     }
-
-    render() {
+    render(){
         const {classes} = this.props;
-        let inviteUsersList =
-            <DialogWindowInvite
-                open={this.state.showInviteBox}
-                onClose={this.handleInviteBoxClose}
-                title={"Select the users you want to invite"}
-                option1={"Invite selected users"}
-                onOption1={this.handleConfirmationDialogOpen}>
-            </DialogWindowInvite>
+        let inviteUsersList = 
+        <DialogWindowInvite 
+            open={this.state.showInviteBox}
+            onClose={this.handleInviteBoxClose}
+            title={"Select the users you want to invite"}
+            option1={"Invite selected users"}
+            onOption1={this.handleConfirmationDialogOpen}>
+        </DialogWindowInvite> 
 
-        let confimarionDialog =
-            <DialogWindow
-                open={this.state.showConfirmationDialog}
-                onClose={this.handleConfirmationDialogClose}
-                title={"You have invited the users to this event!"}>
-            </DialogWindow>
-        return (
-            <div>
-                <Button className={classes.button}
-                        size="small"
+        let confimarionDialog = 
+        <DialogWindow
+            open={this.state.showConfirmationDialog}
+            onClose={this.handleConfirmationDialogClose}
+            title={"You have invited the users to this event!"}>
+        </DialogWindow>
+        return(
+            <div>   
+                    <Button className={classes.button}
+                        size="small" 
                         color={this.state.buttonColor}
                         variant='contained'
                         onClick={this.handleJoinButtonClicked}>
-<<<<<<< Updated upstream
-                    {this.state.buttonText}
-                </Button>
-                {this.state.showInviteButton ?
-                    <Button className={classes.button}
-
-                            size="small"
-                            color="primary"
-                            variant='contained'
-                            onClick={this.handleInviteBoxOpen}>
-=======
                         {this.state.buttonText}
                     </Button>
                     {this.state.showInviteButton ?
@@ -109,15 +93,13 @@ class InviteEvent extends React.Component {
                         color="primary"
                         variant='contained'
                         onClick={this.handleInviteBoxOpen}>
->>>>>>> Stashed changes
                         Invite people
-                    </Button> : null}
-
-                {inviteUsersList}
-                {confimarionDialog}
+                    </Button> :null}
+                    
+                     {inviteUsersList}
+                     {confimarionDialog}
             </div>
         )
     }
 }
-
 export default withStyles(styles)(InviteEvent);
